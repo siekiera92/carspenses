81389e51b1ca499f549f59dcf99ef13a0eef0be8
spogon <szymon.pogon@gmail.com>
2017-04-08 21:36:43 +0200

    usunięcie backendless

--- www/js/app.js
+++ www/js/app.js
@@ -1,4 +1,4 @@
-var APP_ID = '8B647F77-8C3E-D352-FF3E-924B06CD8800';
+/*var APP_ID = '8B647F77-8C3E-D352-FF3E-924B06CD8800';
 var API_KEY = '0B2EDDB0-5FD2-1730-FFB3-F5A006427700';
 
 Backendless.serverURL = 'https://api.backendless.com';
@@ -13,4 +13,4 @@ Backendless.Data.of( "TestTable" ).save( { foo:"bar" } )
         console.log( "got error - " + error )
         alert(error)
     })
-                  
+                  */
