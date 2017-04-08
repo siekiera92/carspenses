33cfb0233283a03ff6d00f8e1e628641ba2fc8bb
spogon <szymon.pogon@gmail.com>
2017-04-08 19:59:11 +0200

    test backendless na telefonie

--- www/js/app.js
+++ www/js/app.js
@@ -4,11 +4,11 @@ var API_KEY = '0B2EDDB0-5FD2-1730-FFB3-F5A006427700';
 Backendless.serverURL = 'https://api.backendless.com';
 Backendless.initApp(APP_ID, API_KEY);
 
-/*Backendless.Data.of( "TestTable" ).save( { foo:"bar" } )
+Backendless.Data.of( "TestTable" ).save( { foo:"bar" } )
     .then( function( obj ) {
         console.log( "object saved. objectId " + obj.objectId )
     } )
     .catch( function( error ) {
         console.log( "got error - " + error )
     })
-                  */  
+                  
