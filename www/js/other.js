fa40f549c36c4e5ab8ed0488ccb72fdd90d3a59a
spogon <szymon.pogon@gmail.com>
2017-04-10 21:39:39 +0200

    przełączanie między ekranami

--- www/js/other.js
+++ www/js/other.js
@@ -1,5 +1,6 @@
 var text = 
-    '<a href="#" class="ui-btn" id="moje-samochody">Moje samochody</a>' +
-    '<a href="#" class="ui-btn">Moje samochody</a>';
+    '<a href="#homepage" class="ui-btn">Moje samochody</a>' +
+    '<a href="#homepage1" class="ui-btn">Moje samochody1</a>';
 
 $('#left-panel').html(text);
+$('#left-panel-1').html(text);
